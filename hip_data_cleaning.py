8a9cc95552813c20d4cce4b736bfb3bc6d5e5363
LoretaE <e.loreta@gmail.com>
2024-07-03 11:19:46 +0300

    Data file and cleaning

---
+++ hip_data_cleaning.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+hipoteka = pd.read_csv('Data/Stsr_AD_hip_ik_skolininkai_nuo_2021.csv', delimiter='|')
+
+# Exploring data (types, missing values, statistical info)
+# print(hipoteka.head())
+# print(hipoteka.describe())
+# print(hipoteka.info())
+
+# Deleting data of credits taken by companies (rows)
+print(hipoteka.imones.unique())
+index_company = hipoteka[hipoteka['imones'] == 1].index
+hipoteka.drop(index_company, inplace=True)
+
+#  Deleting columns with unused data and checking data
+hipoteka.drop(['st_nr', 'imones', 'skol_reg_data', 'st_isreg_ketv', 'pri_terminas_metai', 'formavimo_data',
+               'st_sav_kodas_dm', 'st_aps_kodas', 'skol_amnt_grupe', 'skol_sal_grupe'], axis=1, inplace=True)
+print(hipoteka.describe())
+print(hipoteka.info())
+
+# Exploring data in the columns with NaN values
+print(f'\n NAN values: \n {hipoteka.isna().sum()}')
+
+# Deleting rows with NAN values
+hipoteka = hipoteka.dropna()
+print(hipoteka.info())
+
+# Exploring values of each column
+print('\n Value counts')
+for col in hipoteka.columns:
+    print(hipoteka[col].value_counts())
+
+
+
+#  Exploring data about municipalities, districts
+print(hipoteka.st_sav_aps_pav.value_counts())
+
+#  Dataset includes data from 5 largest municipalities - Vilnius, Kaunas, Klaipeda, Siauliai, Panavezys.
+#  Also, data from 10 districts provided. Therefore, 2 separate dataframes are created for realizing EDA for
+#  5 largest municipalities and 10 districts.
+
+largest_mun = ['Vilniaus m. sav.', 'Kauno m. sav.', 'Klaipėdos m. sav.', 'Šiaulių m. sav.', 'Panevėžio m. sav.']
+hipoteka_mun_5 = hipoteka.loc[hipoteka['st_sav_aps_pav'].isin(largest_mun)]
+print(hipoteka_mun_5.st_sav_aps_pav.value_counts())
+
+hipoteka.replace(to_replace=['Vilniaus m. sav.', 'Kauno m. sav.', 'Klaipėdos m. sav.', 'Šiaulių m. sav.',
+                             'Panevėžio m. sav.'],
+                             value=['Vilniaus apskr.', 'Kauno apskr.', 'Klaipėdos apskr.', 'Šiaulių apskr.',
+                                    'Panevėžio apskr.'], inplace=True)
+print(hipoteka.st_sav_aps_pav.value_counts())
+print(hipoteka.info())
+
+
+
+# sns.boxplot(data=hipoteka, x='skol_amnt_grupe', y='skol_amziaus_grupe', hue='imones')
+# plt.show()
+# sns.boxplot(data=hipoteka, x='imones', y='skol_amziaus_grupe')
+# plt.show()
+
+#  Exploring missing "skol_amziaus_grupe" values, replacing wrong value -
+# print(hipoteka.groupby('imones')['skol_amziaus_grupe'].value_counts())
+
+
