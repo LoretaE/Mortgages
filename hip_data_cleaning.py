afcb59c053d9b2b01f29197593cd6f7d33563690
LoretaE <e.loreta@gmail.com>
2024-07-03 16:38:11 +0300

    Data file and cleaning

--- hip_data_cleaning.py
+++ hip_data_cleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -46,14 +47,29 @@ hipoteka['max_amount'] = hipoteka['max_amount'].replace('nuo 10 tūk. iki 50 tū
 hipoteka['max_amount'] = hipoteka['max_amount'].replace('nuo 50 tūk. iki 100 tūk.', 100000)
 hipoteka['max_amount'] = hipoteka['max_amount'].replace('nuo 100 tūk. iki 500 tūk.', 500000)
 hipoteka['max_amount'] = hipoteka['max_amount'].replace('nuo 500 tūk. Iki 1 mln.', 1000000)
-
 hipoteka['max_amount'] = hipoteka['max_amount'].replace('nuo 1 mln. Iki 5 mln.', 5000000)
 hipoteka['max_amount'] = hipoteka['max_amount'].replace('nuo 5 mln. Iki 10 mln.', 10000000)
 hipoteka['max_amount'] = hipoteka['max_amount'].replace('nuo 10 mln. Iki 50 mln.', 50000000)
 hipoteka['max_amount'] = hipoteka['max_amount'].replace('nuo 50 mln. Iki 100 mln.', 100000000)
 
-print(hipoteka.max_amount.value_counts())
 hipoteka.drop('pri_dydis_rez', axis=1, inplace=True)
+
+#  Detecting outliers
+plt.figure(figsize=(12, 12))
+sns.boxplot(x=hipoteka['skol_amziaus_grupe'], y=hipoteka['max_amount'])
+plt.show()
+
+print(hipoteka.max_amount.value_counts())
+
+#  Removing outliers
+max_amount_out = hipoteka[hipoteka['max_amount'] >= 1000000].index
+hipoteka.drop(max_amount_out, inplace=True)
+print(hipoteka.max_amount.value_counts())
+
+plt.figure(figsize=(12, 6))
+sns.boxplot(x=hipoteka['skol_amziaus_grupe'], y=hipoteka['max_amount'])
+plt.show()
+
 
 #  Exploring data about municipalities, districts
 print(hipoteka.st_sav_aps_pav.value_counts())
@@ -70,5 +86,4 @@ hipoteka.replace(to_replace=['Vilniaus m. sav.', 'Kauno m. sav.', 'Klaipėdos m.
                                     'Panevėžio apskr.'], inplace=True)
 print(hipoteka.st_sav_aps_pav.value_counts())
 print(hipoteka.info())
-
 print(hipoteka.head())
