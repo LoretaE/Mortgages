4c6b96585f1719539730915b5b338c7dc0394ab0
LoretaE <e.loreta@gmail.com>
2024-07-04 12:35:31 +0300

    Data file and cleaning

--- hip_data_categorical.py
+++ hip_data_categorical.py
@@ -20,11 +20,11 @@ for c in col:
 print(hipoteka.info())
 
 #  Converting age category from string to interval[int64]
-age_conv = {'iki_18': 10, 'nuo_18_iki_25': 22, 'nuo_25_iki_35': 30, 'nuo_35_iki_45': 40, 'nuo_45_iki_55': 50,
-            'nuo_55_iki_65': 60, 'nuo_65_iki_85': 75, 'nuo_85': 90}
+age_conv = {'nuo_18_iki_25': 22, 'nuo_25_iki_35': 30, 'nuo_35_iki_45': 40, 'nuo_45_iki_55': 50,
+            'nuo_55_iki_65': 60, 'nuo_65_iki_85': 75}
 hipoteka['age'] = hipoteka['skol_amziaus_grupe'].map(age_conv)
 
-age_bins = [0, 18, 25, 35, 45, 55, 65, 85, 100]
+age_bins = [18, 25, 35, 45, 55, 65, 85]
 age_cats = pd.cut(hipoteka.age, age_bins, right=False)
 hipoteka['age_group'] = age_cats
 print(hipoteka.age_group.value_counts())
